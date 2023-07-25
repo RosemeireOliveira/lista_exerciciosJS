89ed5530d3026976b62658abd5ff6ecefc18b6bb
rosemeire <rstoliveira@gmail.com>
2023-07-24 21:52:05 -0300

    criacao do exercicio typescript

--- exercicios.ts
+++ exercicios.ts
@@ -1,38 +1,45 @@
-import  promptSync from "prompt-sync"
-import { exercicio01, exercicio02,exercicio03,exercicio04, exercicio05, exercicio06 } from "./moduleExercicios.js";
+import promptSync from "prompt-sync"
+import { exercicio01, exercicio02, exercicio03, exercicio04, exercicio05, exercicio06 } from "./moduleExercicios";
+
+// type resultado = number | string
+// const resposta: resultado= 04
 let prompt = promptSync()
-let resposta;
+let resposta: string;
 
 do {
     console.log("Apresentação do exercicio")
-    resposta = prompt("digite o numero  do exercicio desejado ou clique em 'sair'")
+    // resposta: resultado = prompt("digite o numero  do exercicio desejado ou clique em 'sair'")
+    resposta = prompt("digite o numero  do exercicio desejado ou digite 'sair' ")
+
+console.log (resposta)
+
     switch (resposta) {
-        case "1":
+        case '1':
             console.log("você escolheu o exercicio 1")
             exercicio01();
             break;
-    case "2":
+        case "2":
             console.log("você escolheu o exercicio 2")
             exercicio02();
             break;
-            case "3":
+        case "3":
             console.log("você escolheu o exercicio 3")
             exercicio03();
             break;
-            case "4":
+        case "4":
             console.log("você escolheu o exercicio 4")
             exercicio04();
             break;
-            case "5":
+        case "5":
             console.log("você escolheu o exercicio 5")
             exercicio05();
             break;
-            case "6":
+        case "6":
             console.log("você escolheu o exercicio 6")
             exercicio06();
             break;
         default:
-        console.log("\nvalor digitado não conrresponde a nenhum exercicio\n")
+            console.log("\nvalor digitado não conrresponde a nenhum exercicio\n")
             break;
     }
-} while (resposta !='sair')
+} while (resposta != 'sair')
