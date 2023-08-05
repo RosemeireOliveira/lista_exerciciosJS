d2afa56852094f30c7442151115d295aff7685b8
rosemeire <rstoliveira@gmail.com>
2023-08-04 21:09:29 -0300

    exercicio JS Finalizado

--- exercicios.js
+++ exercicios.js
@@ -1,45 +1,38 @@
-import promptSync from "prompt-sync"
-import { exercicio01, exercicio02, exercicio03, exercicio04, exercicio05, exercicio06 } from "./moduleExercicios";
-
-// type resultado = number | string
-// const resposta: resultado= 04
+import  promptSync from "prompt-sync"
+import { exercicio01, exercicio02,exercicio03,exercicio04, exercicio05, exercicio06 } from "./moduleExercicios.js";
 let prompt = promptSync()
-let resposta: string;
+let resposta;
 
 do {
     console.log("Apresentação do exercicio")
-    // resposta: resultado = prompt("digite o numero  do exercicio desejado ou clique em 'sair'")
-    resposta = prompt("digite o numero  do exercicio desejado ou digite 'sair' ")
-
-console.log (resposta)
-
+    resposta = prompt("digite o numero  do exercicio desejado ou clique em 'sair'")
     switch (resposta) {
-        case '1':
+        case "1":
             console.log("você escolheu o exercicio 1")
             exercicio01();
             break;
-        case "2":
+    case "2":
             console.log("você escolheu o exercicio 2")
             exercicio02();
             break;
-        case "3":
+            case "3":
             console.log("você escolheu o exercicio 3")
             exercicio03();
             break;
-        case "4":
+            case "4":
             console.log("você escolheu o exercicio 4")
             exercicio04();
             break;
-        case "5":
+            case "5":
             console.log("você escolheu o exercicio 5")
             exercicio05();
             break;
-        case "6":
+            case "6":
             console.log("você escolheu o exercicio 6")
             exercicio06();
             break;
         default:
-            console.log("\nvalor digitado não conrresponde a nenhum exercicio\n")
+        console.log("\nvalor digitado não conrresponde a nenhum exercicio\n")
             break;
     }
-} while (resposta != 'sair')
+} while (resposta !='sair')
